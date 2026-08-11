ecd768f8210ec7490d52191d19d37600638c6f5d
Thejas Shetty <thejasganiga150503@gmail.com>
2026-08-11 12:39:53 +0530

    Javascript Basics

---
+++ Javascript_For_Testing/basic1.js
@@ -0,0 +1,46 @@
+console.log("Hello World, Welcome to SDET !");
+
+// These are the comments -- Single Line Comment
+
+/* 
+    Multi 
+    Line 
+    Comment 
+*/
+
+// Declaring the variables - var, let, const
+
+let a = 50;
+console.log(a)
+typeof (a); // to see the data type of the variable !
+console.log(typeof (a))
+
+let b = 30.5
+console.log(typeof (b))
+
+let c = "Thejas Shetty"
+console.log(typeof (c))
+
+let required = true;
+console.log(typeof (required))
+
+// null and undefined !
+
+let sum = a + b
+console.log(sum)
+
+// we cannot redeclare the variable with the let keyword but is possible with var !
+
+var name = "Thejas"
+console.log("Name : " + name)
+
+var name = "Shetty"
+console.log("Name : " + name);
+
+// The Not Operator only with boolean !
+
+let bool = true;
+console.log(bool)
+console.log(!bool)
+
+// Use const only when the declaration is constant !
